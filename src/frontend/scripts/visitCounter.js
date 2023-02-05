127026dd403e0e52cb3fc91d672b3c2c5a8d3906
piotr-rzepa <56642437+piotr-rzepa@users.noreply.github.com>
2023-02-05 21:29:16 +0100

    CloudFormation: Add Api Gateway & Lambda
    Add API Gateway with single endpoint for counting visitors
    Add Lambda to interact with DynamoDB and save/retrieve visitors count
    Add implementation of Lambda handler written in Python3.9 using boto3

--- src/frontend/scripts/visitCounter.js
+++ src/frontend/scripts/visitCounter.js
@@ -1,19 +1,18 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
 const p = document.querySelector("#visit-counter");
+VISITORS_ENDPOINT =
+  "https://3q6c09cnfa.execute-api.eu-west-1.amazonaws.com/prod/visitors";
 
-if (typeof Storage !== "undefined") {
-  // Set the counter to 0 if user opens site for the first time
-  if (localStorage.getItem("visits") === null) localStorage.visits = 0;
-} else {
-  console.error("This browser does not support Web Storage.");
-}
-
-const updateCounter = (event) => {
-  const visits = Number.parseInt(localStorage.visits) + 1;
-  localStorage.visits = visits;
-  p.textContent = `This page has been visited ${visits} times`;
-  console.log(p.textContent);
+const updateCounter = async (event) => {
+  const response = await fetch(VISITORS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const visitors = await response.json();
+  return visitors.visits;
 };
 
 window.addEventListener("DOMContentLoaded", updateCounter);
