6496aa91688b443282cfe40ec410b6a099ee83a0
piotr-rzepa <56642437+piotr-rzepa@users.noreply.github.com>
2023-02-16 19:18:47 +0100

    Frontend: Add Cypress smoke testing
    Add Cypress smoke testing to GithubActions
    Smoke tests are loaded after
    successful infrastructure deployment
    Add test cases to verify page is reachable
    Add test cases to visitors counter is updated correctly

---
+++ src/frontend/tests/cypress.config.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 8 */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  video: false,
+});
